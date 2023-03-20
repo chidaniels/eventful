9e779c6326fc378f39bf998a764e03820740034e
chidaniel <61457861+chidaniels@users.noreply.github.com>
2023-03-20 15:27:27 +0100

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+      <footer class="container bg-[#60CED4] border-[#60CED5] mt-20 text-right m-auto w-full px-8 py-3">
+        <p class="pr-15">Eventful Moments by chidaniels</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name:'Footer'
+}
+</script>
