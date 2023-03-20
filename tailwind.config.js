0f22d3aac62d9377fef1055a16b5471a88d16913
chidaniel <61457861+chidaniels@users.noreply.github.com>
2023-03-20 15:28:38 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      'tablet':'640px',
+      'laptop':'1024px',
+      'desktop':'1280px'
+    },
     extend: {},
   },
   plugins: [],
