0b48ffba9a7a1a00055f9750022ebc09164bdba4
chidaniel <61457861+chidaniels@users.noreply.github.com>
2023-03-21 18:32:20 +0100

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="m-auto bg-white border-solid border-1 border-gray-500 bg-gray-50 w-4/5 h-4/5">
+    <div class="m-auto  border-solid border-1 border-gray-500 bg-gray-50 w-4/5 h-4/5">
     <Header/>
      <div class="mx-auto ">
         <h2 class="text-3xl w-2/5 font-bold m-auto mt-20">Welcome back,</h2>
@@ -13,14 +13,14 @@
         <form class="mt-10">
             <div class="flex flex-col w-2/5 m-auto">
                 <label>Email</label>
-                <input type="email" name="email" input class="mr-15 border-solid border-2 border-gray-300  rounded-lg block" >
+                <input type="email" v-model="email" name="email" input class="mr-15 border-solid border-2 border-gray-300  rounded-lg block" >
             </div>
             <div class="flex flex-col w-2/5 m-auto mt-8">
                 <label>Password</label>
-                <input type="password" name="password" class=" mr-15 border-solid border-2 border-gray-300  rounded-lg block ">
+                <input type="password" v-model="password" name="password" class=" mr-15 border-solid border-2 border-gray-300  rounded-lg block ">
             </div>
             <router-link to="/bucket">
-                <button type="submit" class="bg-[#5271FF] text-white rounded-lg block mt-10 mx-auto text-sm px-7 py-2" >Login</button>
+                <button type="submit" v-on:click="logIn" class="bg-[#5271FF] text-white rounded-lg block mt-10 mx-auto text-sm px-7 py-2" >Login</button>
             </router-link>
         </form>
     </div>
@@ -29,6 +29,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
 
@@ -37,6 +39,36 @@ export default {
 components: {
     Header,
     Footer
+},
+data()
+{
+    return {
+        email:"",
+        password:""
+    }
+},
+methods: {
+   async logIn()
+    {
+        let result = await axios.post("https://eventful-moments.onrender.com/api/v1/users/login", {
+            email:this.email,
+            password:this.password
+        });
+        console.warn(result);
+        if (result.status==200)
+        {
+            localStorage.setItem("user-info",JSON.stringify(result.data))
+            this.$router.push({name:"Bucket"})
+        }
+    }
+},
+mounted()
+{
+   let user=localStorage.getItem("user-info")
+   if(user){
+        this.$router.push({name:"Bucket"})
+     }
 }
+
 }
 </script>
